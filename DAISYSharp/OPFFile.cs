dce70b185c7418e4215974e15924597ad767edf0
garrettk18 <garrett.gj@gmail.com>
2014-05-23 20:10:12 -0400

    Basic skeletin of OPFFile class.

---
+++ DAISYSharp/OPFFile.cs
@@ -0,0 +1,16 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Xml;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace DAISYSharp
+{
+    public class OPFFile
+    {
+        public string _FileName { get; set; }
+        private XmlDocument _opfDom;
+        public List<string> _FileList { get; set; }
+    }
+}
